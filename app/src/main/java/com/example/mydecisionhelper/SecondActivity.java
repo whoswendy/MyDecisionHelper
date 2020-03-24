e7c22e4c42428fd97c6fd0e26f813a7764372797
whoswendy <wendy.hu@stonybrook.edu>
2020-03-24 00:14:32 -0400

    Decision maker works! Added Snackbar and imports to show decision after clicking button
    Problem: when the keyboard pops up cant see top of screen

--- app/src/main/java/com/example/mydecisionhelper/SecondActivity.java
+++ app/src/main/java/com/example/mydecisionhelper/SecondActivity.java
@@ -3,15 +3,19 @@ package com.example.mydecisionhelper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.support.design.widget.Snackbar;
+
 
 import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     private String[] listChoices;
+    String decision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +28,16 @@ public class SecondActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 getChoices(editText);
-                makeDecision();
-                goToDecision();
+                decision = makeDecision();
+                goToDecision(decision);
             }
         });
     }
 
     private void getChoices(EditText editText){
         String text = editText.getText().toString();
-        listChoices = text.split("/n");
+        Log.d("Input",text);
+        listChoices = text.split("\n");
     }
 
     public String[] getListChoices(){
@@ -45,8 +50,8 @@ public class SecondActivity extends AppCompatActivity {
         return listChoices[random];
     }
 
-    private void goToDecision(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+    private void goToDecision(String decision) {
+        Snackbar mySnackbar = Snackbar.make(findViewById(R.id.myCoordinatorLayout), decision, Snackbar.LENGTH_LONG);
+        mySnackbar.show();
     }
 }
